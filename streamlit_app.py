746a57cfc54a534b6a03e7432b2cf8db394c24a2
Filipe Brandão <fdabrandao@gmail.com>
2023-01-11 18:53:18 +0000

    Implement N-Queens with a slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,23 +10,47 @@ uuid = os.environ.get("AMPLKEY_UUID")
 if uuid is not None:
     tools.activate_license(uuid, verbose=True)
 
+
+"""
+# N-Queens
+ 
+**How can N queens be placed on an NxN chessboard so that no two of them attack each other?**
+
+
+## Modeling N-Queens with `alldiff`
+
+Constraint `alldiff` enforces a set of integer variables to take distinct values. Using `alldiff`, we can model N-Queens as follows:
+
+```ampl
+param n integer > 0; # N-queens
+var Row {1..n} integer >= 1 <= n;
+s.t. row_attacks: alldiff ({j in 1..n} Row[j]);
+s.t. diag_attacks: alldiff ({j in 1..n} Row[j]+j);
+s.t. rdiag_attacks: alldiff ({j in 1..n} Row[j]-j);
+```
+"""
+
 ampl = AMPL()
 ampl.eval(
     """
-set I := 1..1000;
-var x{I} binary;
-param v{i in I} := 1 + Irand224() mod 1000;
-param w{i in I} := 1 + Irand224() mod 1000;
-maximize profit: sum{i in I} v[i] * x[i];
-s.t. capacity: sum{i in I} w[i] * x[i] <= 1000;
+param n integer > 0; # N-queens
+var Row {1..n} integer >= 1 <= n;
+s.t. row_attacks: alldiff ({j in 1..n} Row[j]);
+s.t. diag_attacks: alldiff ({j in 1..n} Row[j]+j);
+s.t. rdiag_attacks: alldiff ({j in 1..n} Row[j]-j);
 """
 )
 ampl.option["solver"] = "highs"
+ampl.option["highs_options"] = "outlev=0"
 
-## Version
+n = st.slider("How many queens?", 2, 25, 8)
 
-st.write(ampl.option["version"])
-
-## Solve
-
-st.write(ampl.get_output("solve;"))
+ampl.param["n"] = n
+ampl.solve()
+solution = ampl.get_data("Row").to_dict()
+queens = set((int(r) - 1, int(c) - 1) for c, r in solution.items())
+st.write("# Solution")
+for r in range(n):
+    st.write(
+        "`" + "".join([" Q " if (r, c) in queens else " + " for c in range(n)]) + "`"
+    )
